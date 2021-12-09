70db6a5c4a891704d3f48466efb941a2df227d24
surajnet <92844123+surajnet@users.noreply.github.com>
2021-12-10 03:41:25 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,18 +6,18 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "8b5f0b8e497484f7604f333baa4fbc5b",
+"assets/NOTICES": "86ca0019ad8c7bf32db6aff131bee1a1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4289115128364e7862c11faf8eb48005",
-"/": "4289115128364e7862c11faf8eb48005",
-"main.dart.js": "d41afd230415daf826a20976fa571cfc",
-"manifest.json": "0042414ffb1a84ffe4477b8112dfbd08",
-"version.json": "0ae889e301b8ce15020379b548dbeb41"
+"index.html": "c48da6fe5869801861907d27ee313988",
+"/": "c48da6fe5869801861907d27ee313988",
+"main.dart.js": "6942a7bbbb2aaa4ff8d1b736a8a49de6",
+"manifest.json": "dde60d46fd9aedece2072f82670fa19d",
+"version.json": "8ace620354c9382217960f0ef87d63f2"
 };
 
 // The application shell files that are downloaded before a service worker can
